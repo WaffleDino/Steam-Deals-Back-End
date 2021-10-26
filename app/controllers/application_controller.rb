faff21739017db9f975b80a1387cfbb56c0ad807
sandy <sandyks.lo27@gmail.com>
2021-10-26 14:51:40 -0700

    started sinatra during startup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,14 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
+  # localhost:9292
   get "/" do
     { message: "Good luck with your project!" }.to_json
   end
 
+  # http://localhost:9292/users
+  get '/users' do
+    User.all.to_json
+  end
 end
+
